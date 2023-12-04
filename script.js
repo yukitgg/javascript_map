9ec638b1dce6f6efe98b3305e1a0f7faae333bc7
yukitgg <107914654+yukitgg@users.noreply.github.com>
2023-12-04 13:56:31 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// JavaScript
+
+const map = L.map('map').setView([33.66719252628797, 130.44390437674102], 15);
+
+// タイルレイヤーを作成し、地図にセットする
+
+// Open Street Map
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// 国土地理院
+// L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
+//   maxZoom: 18,
+//   attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院</a>',
+// }).addTo(map);
+
+//アイコン
+// const whiteIcon = L.icon({
+//   iconUrl: 'ico.png',
+//   shadowUrl: 'ico_shadow.png',
+// 
+// iconSize:     [40, 40], // size of the icon
+// shadowSize:   [40, 40], // size of the shadow
+// iconAnchor:   [20, 40], // point of the icon which will correspond to marker's location
+// shadowAnchor: [20, 40],  // the same for the shadow
+// popupAnchor:  [0, 42] // point from which the popup should open relative to the iconAnchor
+// });
+
+//複数アイコンをまとめて定義
+const circleIcon = L.Icon.extend({
+  options: {
+    shadowUrl: 'images/ico_shadow.png',
+    iconSize: [40, 40],
+    shadowSize: [40, 40],
+    iconAnchor: [20, 40],
+    shadowAnchor: [20, 40],
+    popupAnchor: [0, -42]
+  }
+});
+
+const whiteIcon = new circleIcon({ iconUrl: 'images/ico.png' }),
+  pinkIcon = new circleIcon({ iconUrl: 'images/ico_pink.png' });
+
+L.marker([33.67345752177457, 130.44140661378802], { icon: whiteIcon }).addTo(map)
+  .bindPopup('A pretty CSS popup.<br> Easily customizable.<br><img src="images/img01.png" alt="img">');
+// .openPopup();
+// openPopupの追加で最初から吹き出し表示
+
+L.marker([33.65958150849491, 130.4440143454105], { icon: pinkIcon }).addTo(map)
+  .bindPopup('A pretty CSS popup.<br> Easily customizable.');
+
+L.marker([33.67020170623659, 130.4345714724636], { icon: pinkIcon }).addTo(map)
+  .bindPopup('A pretty CSS popup.<br> Easily customizable.');
